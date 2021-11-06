9694c97c4f07dad164b0608c41d8c2b7d8276d2e
mahmoud amr nabil <manamw222@gmail.com>
2021-11-06 11:09:17 +0200

    add main fragments

---
+++ app/src/main/java/com/example/nottumbler/fragments/HomeFragment.java
@@ -0,0 +1,22 @@
+package com.example.nottumbler.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.nottumblr.R;
+
+public class HomeFragment extends Fragment {
+
+    private View root;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        root = inflater.inflate(R.layout.fragment_home, container, false);
+        return root;
+    }
+}
---
+++ app/src/main/java/com/example/nottumbler/fragments/SearchFragment.java
@@ -0,0 +1,22 @@
+package com.example.nottumbler.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.nottumblr.R;
+
+public class SearchFragment extends Fragment {
+
+    private View root;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        root = inflater.inflate(R.layout.fragment_search, container, false);
+        return root;
+    }
+}
