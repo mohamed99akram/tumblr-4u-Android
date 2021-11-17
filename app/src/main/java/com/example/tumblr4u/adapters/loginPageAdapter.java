e93cbf21c110136468838fd3d51c760361dadb61
Omar-ahmed314 <omar.ahmed314@hotmail.com>
2021-11-17 18:30:48 +0200

    added the fragments in the authentication page

---
+++ app/src/main/java/com/example/tumblr4u/adapters/loginPageAdapter.java
@@ -0,0 +1,39 @@
+package com.example.tumblr4u.adapters;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import com.example.tumblr4u.fragments.LoginFragment1;
+import com.example.tumblr4u.fragments.LoginFragment2;
+import com.example.tumblr4u.fragments.LoginFragment3;
+
+public class loginPageAdapter extends FragmentStateAdapter{
+
+    public loginPageAdapter(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle) {
+        super(fragmentManager, lifecycle);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+
+        switch(position) {
+            case 0:
+                return new LoginFragment1();
+            case 1:
+                return new LoginFragment2();
+            case 2:
+                return new LoginFragment3();
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return 3;
+    }
+}
--- app/src/main/java/com/example/tumblr4u/authentication.java
+++ app/src/main/java/com/example/tumblr4u/authentication.java
@@ -1,14 +1,34 @@
 package com.example.tumblr4u;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.example.tumblr4u.adapters.loginPageAdapter;
 
 public class authentication extends AppCompatActivity {
+
+    Button loginButton;
+    Button signupButton;
+    ViewPager2 viewPager;
+    loginPageAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_authentication);
+        getSupportActionBar().hide();
+
+        loginButton = (Button)findViewById(R.id.login_button);
+        signupButton = (Button)findViewById(R.id.signup_button);
+        viewPager = (ViewPager2)findViewById(R.id.login_viewPager);
+        adapter = new loginPageAdapter(getSupportFragmentManager(), getLifecycle());
+
+        viewPager.setAdapter(adapter);
+
     }
 }
