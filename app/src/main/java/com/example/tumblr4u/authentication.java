045cbd4c94d0c9474aaaea60fce0da1797a05067
Omar-ahmed314 <omar.ahmed314@hotmail.com>
2021-11-17 16:19:58 +0200

    creating the login page buttons

---
+++ app/src/main/java/com/example/tumblr4u/authentication.java
@@ -0,0 +1,14 @@
+package com.example.tumblr4u;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class authentication extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authentication);
+    }
+}
