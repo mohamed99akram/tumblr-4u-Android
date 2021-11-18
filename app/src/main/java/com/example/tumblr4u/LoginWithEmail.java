1a8e5ae89f1babe6ef4f64dd51bf7d4003e89adb
Omar-ahmed314 <omar.ahmed314@hotmail.com>
2021-11-18 19:46:13 +0200

    added the initial authentication to the login with email page

--- app/src/main/java/com/example/tumblr4u/LoginWithEmail.java
+++ app/src/main/java/com/example/tumblr4u/LoginWithEmail.java
@@ -1,9 +1,14 @@
 package com.example.tumblr4u;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,9 +18,9 @@ public class LoginWithEmail extends AppCompatActivity {
 
     private Button mLogin;
     private ImageButton mBack;
-    private Button mContinue;
     private EditText mEmailField;
     private EditText mPasswordField;
+    private LoginWithEmailViewModel mViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,9 +29,10 @@ public class LoginWithEmail extends AppCompatActivity {
 
         mLogin = (Button)findViewById(R.id.login_with_email_login_button);
         mBack = (ImageButton) findViewById(R.id.login_with_email_back_button);
-        mContinue = (Button)findViewById(R.id.login_with_email_continue_button);
         mEmailField = (EditText) findViewById(R.id.login_with_email_email_field);
         mPasswordField = (EditText) findViewById(R.id.login_with_email_password_field);
+        mViewModel = new ViewModelProvider(this).get(LoginWithEmailViewModel.class);
+        mLogin.setEnabled(false);
 
         // set the buttons event listner
         mBack.setOnClickListener(new View.OnClickListener() {
@@ -40,8 +46,56 @@ public class LoginWithEmail extends AppCompatActivity {
         mLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent homeActivity = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(homeActivity);
+                mViewModel.login(mEmailField.getText().toString(), mPasswordField.getText().toString());
+            }
+        });
+
+        mViewModel.isValidEmailAndPassword.observe(this, new Observer<Boolean>() {
+            @Override
+            public void onChanged(Boolean aBoolean) {
+                if(!mViewModel.isValidEmailAndPassword.getValue()) { return; }
+                Intent home = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(home);
+            }
+        });
+
+        mEmailField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+                mLogin.setEnabled(!(mEmailField.getText().toString().equals("") |
+                        mPasswordField.getText().toString().equals("")));
+
+            }
+        });
+
+        mPasswordField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+
+                mLogin.setEnabled(!(mEmailField.getText().toString().equals("") |
+                        mPasswordField.getText().toString().equals("")));
             }
         });
     }
--- app/src/main/java/com/example/tumblr4u/LoginWithEmailViewModel.java
+++ app/src/main/java/com/example/tumblr4u/LoginWithEmailViewModel.java
@@ -1,6 +1,18 @@
 package com.example.tumblr4u;
 
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class LoginWithEmailViewModel extends ViewModel {
+
+    public MutableLiveData<Boolean> isValidEmailAndPassword = new MutableLiveData<>();
+
+    public void login(String email, String password){
+
+        isValidEmailAndPassword.setValue(dataBaseLogin(email, password));
+    }
+
+    private Boolean dataBaseLogin(String email, String password){
+        return true;
+    }
 }
