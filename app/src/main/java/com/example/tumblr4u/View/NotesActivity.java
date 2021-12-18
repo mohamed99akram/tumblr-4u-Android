739381e57e014c6150d68cf0ffd07a7f2eca9693
Mohamed Akram <mohamed1999akram@gmail.com>
2021-12-19 00:55:04 +0200

    comments xml page not completed

---
+++ app/src/main/java/com/example/tumblr4u/View/NotesActivity.java
@@ -0,0 +1,16 @@
+package com.example.tumblr4u.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.tumblr4u.R;
+
+public class NotesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notes);
+    }
+}
