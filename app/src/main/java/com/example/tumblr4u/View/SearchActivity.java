18da0c61916c2ff1d077260c2e1da34f6fc7a488
omar-ahmed314 <omar.ahmed314@hotmail.com>
2021-12-28 00:14:01 +0200

    add multiple files
    add two fragments at the searching page
    one for the suggestions and one for the results
    
    add recycler view for the suggestion layout
    add the recycler view item

--- app/src/main/java/com/example/tumblr4u/View/SearchActivity.java
+++ app/src/main/java/com/example/tumblr4u/View/SearchActivity.java
@@ -1,14 +1,52 @@
 package com.example.tumblr4u.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.example.tumblr4u.Fragments.SearchResultPageFragment;
+import com.example.tumblr4u.Fragments.SearchSuggestionPageFragment;
 import com.example.tumblr4u.R;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class SearchActivity extends AppCompatActivity {
+
+    private SearchSuggestionPageFragment mSuggestionPage;
+    private SearchResultPageFragment mResultPage;
+    private ImageButton mBackButton;
+    private EditText mSearchField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        initViews();
+        initOnClickListners();
+
+        // set the fragment manager
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.search_page_fragment_layout, mSuggestionPage, "suggestionPage")
+                .commit();
+    }
+
+    private void initViews(){
+        mSuggestionPage = new SearchSuggestionPageFragment();
+        mResultPage = new SearchResultPageFragment();
+        mBackButton = (ImageButton) findViewById(R.id.search_page_back_button);
+        mSearchField = (EditText) findViewById(R.id.search_page_search_field);
+    }
+    private void initOnClickListners(){
+        mBackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
