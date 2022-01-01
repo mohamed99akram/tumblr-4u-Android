657e8d592232b36eff9e47dd5cc15bab83f68abd
omar-ahmed314 <omar.ahmed314@hotmail.com>
2022-01-01 15:51:21 +0200

    add the login and signup response javaDoc

--- app/src/main/java/com/example/tumblr4u/ApiData/Login_Signup/LoginSignupData.java
+++ app/src/main/java/com/example/tumblr4u/ApiData/Login_Signup/LoginSignupData.java
@@ -3,6 +3,14 @@ package com.example.tumblr4u.ApiData.Login_Signup;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
+/**
+ * Response Data for the login and signup that contains token
+ * json version
+ * {
+ *      "token": "LKFAJJOI76DFJJJSLKDFJOJF"
+ * }
+ * @version 1.1
+ * */
 public class LoginSignupData {
     @SerializedName("token")
     @Expose
